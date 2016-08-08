7ca772fab3756a2185114f69d29c2bd68a512c3e
morganhowell95 <morgan.howell95@gmail.com>
2016-08-07 22:01:37 -0700

    updates to gallery

--- app/assets/javascripts/blog.js
+++ app/assets/javascripts/blog.js
@@ -6,47 +6,58 @@ $(".blog.main").ready(
 			$('#introvid').addClass('hidden');
 			$('#wrapper').addClass('hidden');
 			$('.menu-info').addClass('hidden');
+			$('.wrapper').addClass('hidden');
+		}
+
+		var inverseMenuGeneral = function() {
+			$('.menu').addClass('hidden');
 		}
 
 		
-		$('.hm').click(function(){
+		$('#algo').click(function(){
 			inverseMain();
 			$('#dsa-wrapper').toggleClass('hidden');
 		});
 
 
-		$('.fb').click(function(){
+		$('#interview').click(function(){
 			inverseMain();
 			$('#ips-wrapper').toggleClass('hidden');
 		});
 
 
-		$('.gp').click(function(){
+		$('#clrs').click(function(){
 			inverseMain();
 			$('#clrs-wrapper').toggleClass('hidden');
 		});
 
 
-		$('.tw').click(function(){
+		$('#workout').click(function(){
 			inverseMain();
 			$('#workout-wrapper').toggleClass('hidden');
 		});
+
+		//algo content
+		$('#qs').click(function(){
+			inverseMenuGeneral();
+			$('#queues-stacks-wrapper').removeClass('hidden');
+		})
 		
 		$('.back-button').click(function(){
 			$('#workout-wrapper').addClass('hidden');
 			$('#dsa-wrapper').addClass('hidden');
 			$('#ips-wrapper').addClass('hidden');
 			$('#clrs-wrapper').addClass('hidden');
+			$('#queues-stacks-wrapper').addClass('hidden');
 			$('#introvid').removeClass('hidden');
 			$('#wrapper').removeClass('hidden');
 			$('.menu-info').removeClass('hidden');
+			$('.menu').removeClass('hidden');
 		});
 
 		$('#menu-info').click(function(){
 			$('#introvid').toggleClass('hidden');
 			$('#modal9').modal('show');
 		})
-
-
 
 });
